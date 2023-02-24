cc2aadc6139493c448a6b0db1c639d4fc9c4cdd6
alvinist27 <suhrobhotamov496@gmail.com>
2023-02-24 23:35:50 +0300

    Fixed reflected xss scanning (added requests)

--- scanner/process.py
+++ scanner/process.py
@@ -1,6 +1,7 @@
 from typing import Set
 from urllib.parse import urljoin, urlparse
 
+import requests
 import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
@@ -24,7 +25,6 @@ class ScanProcess:
         inputs = []
         for input_field in form.find_all('input'):
             inputs.append({
-                'id': input_field.attrs.get('id', ''),
                 'name': input_field.attrs.get('name', ''),
                 'type': input_field.attrs.get('type', 'text'),
             })
@@ -38,9 +38,9 @@ class ScanProcess:
         urls = set()
         domain_name = urlparse(url).netloc
         self.driver.get(url)
-        page_source = BeautifulSoup(self.driver.page_source, 'html.parser')
-        for a_tag in page_source.findAll('a'):
-            href = a_tag.attrs.get('href')
+        a_tags = self.driver.find_elements(By.CSS_SELECTOR, 'a')
+        for a_tag in a_tags:
+            href = a_tag.get_attribute('href')
             if not href:
                 continue
             parsed_href = urlparse(urljoin(url, href))
@@ -68,28 +68,31 @@ class ScanProcess:
         return page_content.find_all('form')
 
     def submit_form(self, form, form_info, script):
-        urljoin(self.target_url, form_info['action'])
-        input_css_selector = ''
-        for input in form_info['inputs']:
-            input_id, input_name = input['id'], input['name']
-            input_css_selector = f'input[name="{input_name}"], input[id="{input_id}"]'
-            if input['type'] in ('text', 'search') and (input['id'] or input['name']):
-                self.driver.find_element(By.CSS_SELECTOR, input_css_selector).send_keys(script)
-        form_selector = f'form:has({input_css_selector})'
-        self.driver.find_element(By.CSS_SELECTOR, form_selector).submit()
+        scan_url = urljoin(self.target_url, form_info['action'])
+        data = {}
+        for form_input in form_info['inputs']:
+            if form_input['type'] in ('text', 'search') and form_input['name']:
+                data[form_input['name']] = script
+        if form_info['method'] == 'post':
+            return requests.post(scan_url, data=data)
+        return requests.get(scan_url, params=data)
 
     def scan_reflected_xss(self):
-        vulnerable_urls = []
+        vulnerable_urls = set()
         self.create_sitemap(self.target_url)
-        scripts = ['<script>alert("XSS")</script>']
+        scripts = [
+            '<script>alert("XSS")</script>',
+            '"><script>alert("xss")</script>',
+            '"%3cscript%3ealert(document.cookie)%3c/script%3e',
+        ]
         for url in self.internal_urls:
-            forms = self.get_page_forms(url)
-            for form in forms:
+            page_forms = self.get_page_forms(url)
+            for form in page_forms:
                 form_info = self.get_form_info(form)
                 for script in scripts:
-                    self.submit_form(form, form_info, script)
-                    if script in self.driver.page_source:
-                        vulnerable_urls.append(url)
+                    submit_response = self.submit_form(form, form_info, script).content.decode()
+                    if script in submit_response:
+                        vulnerable_urls.add(url)
                         break
         return vulnerable_urls
 
@@ -97,4 +100,3 @@ class ScanProcess:
 if __name__ == '__main__':
     scan = ScanProcess('http://testphp.vulnweb.com/')
     print(scan.scan_reflected_xss())
-    print(1)
